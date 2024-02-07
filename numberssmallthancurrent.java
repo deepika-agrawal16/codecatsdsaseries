1991d7c05a4a909e228e752d11d2cf814ba823b8
Deepika Agrawal <142203258+deepika-agrawal16@users.noreply.github.com>
2024-02-08 00:34:50 +0530

    Add files via upload

---
+++ numberssmallthancurrent.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+       
+       int result [] = new int[nums.length];
+       for(int i=0;i<nums.length;i++){
+           for(int j=i+1;j<nums.length;j++){
+               if(nums[j]<nums[i]){
+                   result[i] = result[i] +1;
+               }
+              else if(nums[j]!=nums[i]){
+                   result[j] = result[j]+1;
+               }
+           }
+       }
+        return result;
+
+    }
+}
