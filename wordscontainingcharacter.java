23f612af840637218fb483431a403cb1ecd4e579
Deepika Agrawal <142203258+deepika-agrawal16@users.noreply.github.com>
2024-02-08 00:50:19 +0530

    Add files via upload

---
+++ wordscontainingcharacter.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<words.length;i++){
+          if(words[i].contains("" + x))
+            {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
